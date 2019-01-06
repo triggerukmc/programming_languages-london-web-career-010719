1ead964f3eff295d29bbf31cf978897921ad65b2
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-06 17:57:52 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
+
+
+
+
+
 def reformat_languages(languages)
-  # your code here
+ 
+  new_hash={}
+  languages.each do |style, lang|
+    lang.each do |lang2, type|
+        if new_hash[lang2] == nil
+        new_hash[lang2] = {
+          type: type[:type],
+          style: [style]}
+        
+        else
+          new_hash[lang2][:style] << style
+    end
+  end
+end
 end
