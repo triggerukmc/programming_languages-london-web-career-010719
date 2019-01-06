a2bb7c5717532ca367ed9e663ea8bd3925d8a399
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-06 18:06:42 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,7 @@
 
 def reformat_languages(languages)
  
-   new_hash = Hash.new
+   new_hash = {}
   languages.each do |style, styleinfo|
     styleinfo.each do |lan,laninfo|
       if new_hash.has_key?(lan)
